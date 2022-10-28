bd85d50eec252e0abe3fe938b011fddf11194a15
bfelia <83095154+bfelia@users.noreply.github.com>
2022-10-27 23:48:12 -0300

    Add files via upload

---
+++ search/script.js
@@ -0,0 +1,24 @@
+const searchInput = document.querySelector("[data-search]")
+const caseName = document.querySelector("[data-case-name]")
+
+let cases = []
+
+searchInput.addEventListener("input", (e) => {
+    const value = e.target.value
+    console.log(value)
+})
+
+const basket = document.getElementById("basketball");
+const brandcollage = document.getElementById("brandcollage");
+const stevejobs = document.getElementById("stevejobs");
+const xxxtentacion = document.getElementById("xxxtentacion");
+const nasa = document.getElementById("nasa");
+const wizkhalifa = document.getElementById("wizkhalifa");
+const jordanair = document.getElementById("jordanair");
+const barrawonka = document.getElementById("barrawonka");
+const dibujosmujer = document.getElementById("dibujosmujer");
+const hodl = document.getElementById("hodl");
+const gym = document.getElementById("gym");
+const maradona = document.getElementById("maradona");
+
+
